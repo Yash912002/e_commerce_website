cba7689792babccd1b6dc4295ae48faabdf01a53
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-01-07 23:45:47 +0530

    Removed "any" and made the quantity field optional

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,45 +3,51 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 type TransformedItemProps = {
-  price_data: {
-    currency: string;
-    product_data: {
-      name: string;
-    };
-    unit_amount: number;
-  };
-  quantity: number;
+	price_data: {
+		currency: string;
+		product_data: {
+			name: string;
+		};
+		unit_amount: number;
+	};
+	quantity?: number;
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-12-18.acacia",
+	apiVersion: "2024-12-18.acacia",
 });
 
 export async function POST(req: Request) {
-  try {
-    const { items } :  { items: productsProps[] } = await req.json();
+	try {
+		const { items }: { items: productsProps[] } = await req.json();
 
-    const transformedItems: TransformedItemProps[] = items.map((item: any) => ({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.title,
-        },
-        unit_amount: item.price * 100, // Convert to cents
-      },
-      quantity: item.quantity,
-    }));
+		const transformedItems: TransformedItemProps[] = items.map((item) => ({
+			price_data: {
+				currency: "usd",
+				product_data: {
+					name: item.title,
+				},
+				unit_amount: item.price * 100, // Convert to cents
+			},
+			quantity: item?.quantity,
+		}));
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: transformedItems,
-      mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cancel`,
-    });
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ["card"],
+			line_items: transformedItems,
+			mode: "payment",
+			success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+			cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cancel`,
+		});
 
-    return NextResponse.json({ sessionId: session.id });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
+		return NextResponse.json({ sessionId: session.id });
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			return NextResponse.json({ error: err.message }, { status: 500 });
+		}
+		return NextResponse.json(
+			{ error: "An unknown error occurred" },
+			{ status: 500 }
+		);
+	}
 }
